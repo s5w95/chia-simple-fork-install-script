201b61fd0d7b0bd5e1ad17d529bee0556b0e9dd2
s5w95 <sascha.waeschle@d4ho.de>
2021-08-07 04:35:51 +0200

    Add harvester mode (wip)

--- update-nodes.sh
+++ update-nodes.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+#set mode to harvester or node
+MODE=harvester
+#set node ip only needed on harvester mode
+NODE_IP=192.168.9.107
+NODE_USER=dev
+
 BASEDIR=$(dirname "$0")
 . $BASEDIR/forks.config
 
+
+if [ $MODE = harvester ]; then
+	#download certs from node
+	rm ~/blockchain-certs -rf
+	scp -r $NODE_USER@$NODE_IP:~/blockchain-certs ~/blockchain-certs
+fi
+
 for index in "${!forks[@]}"
 do
-echo 
 	fork=${forks[$index]}
 	profileAvailable=false
 
@@ -44,25 +56,45 @@ echo
 	#jump into the env
 	. ./activate
 	
-	#start init
-	$fork init
-	
-	if [ $profileAvailable = false ] ; then
-		#add private key manually after you added a new fork, I dont want to set it for security reasons but feel free todo and pipe it
-		$fork keys add
-		#pipe example for autofill:
-		#echo "some key words right here" | $fork keys add
+	if [ $MODE = harvester ]; then
+		#start init with node certs no keys needed here
+		$fork init -c ~/blockchain-certs/$fork/
+		
+		if [ $profileAvailable = false ] ; then
+			for plotDir in "${plotDirs[@]}"
+			do
+				$fork plots add -d $plotDir
+			done
+		fi
+		
+		$fork start harvester
+		
+		#todo insert node ipp on harvester config.yaml
+	else
+		#start init
+		$fork init
+		
+		if [ $profileAvailable = false ] ; then
+			#add private key manually after you added a new fork, I dont want to set it for security reasons but feel free todo and pipe it
+			$fork keys add
+			#pipe example for autofill:
+			#echo "some key words right here" | $fork keys add
+		fi
+		
+		#start node without timelord
+		$fork start harvester farmer node wallet
+		
+		if [ $profileAvailable = false ] ; then
+			#waiting for wallet startup for wallet init, maybe needs adjustment?
+			sleep 1m
+			echo "S" | $fork wallet show
+		fi
+		
+		if [ -d ~/blockchain-certs ]; then
+			rm ~/blockchain-certs/$fork -rf
+			mkdir ~/blockchain-certs/$fork
+			cp ~/.${forksProfile[$index]}/mainnet/config/ssl/ca/* ~/blockchain-certs/$fork/
+		fi
 	fi
-    
-	#start node without timelord
-	$fork start harvester farmer node wallet
-	
-	if [ $profileAvailable = false ] ; then
-		#waiting for wallet startup for wallet init, maybe needs adjustment?
-		sleep 1m
-		echo "S" | $fork wallet show
-	fi
-	
-	#send some telegram message
-	telegram-send "*$fork* node updated and is now starting" --format markdown
+   
 done
